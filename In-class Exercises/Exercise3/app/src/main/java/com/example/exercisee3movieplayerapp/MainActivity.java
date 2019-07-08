dbd6e8e4dc879381ce9f689b26f42a92d0dea344
Joseph Jeno <joseph.jeno@gmail.com>
2019-07-08 12:31:17 -0400

    intial build working

---
+++ In-class Exercises/Exercise3/app/src/main/java/com/example/exercisee3movieplayerapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.exercisee3movieplayerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Step 1 : Link the video view from xml to activity
+    VideoView video;
+
+    // Step 2 : Assign the video clip to the video view
+
+
+    // Step 3 : Play the video
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        video = findViewById(R.id.myVideoView);
+
+        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.harrypotter);
+
+        video.start();
+    }
+}
