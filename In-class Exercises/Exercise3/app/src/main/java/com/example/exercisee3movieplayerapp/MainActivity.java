b517d6ce2ddc49e94f96443f24d012f4b880c349
Joseph Jeno <joseph.jeno@gmail.com>
2019-07-09 12:20:24 -0400

    keep video position on rotate

--- In-class Exercises/Exercise3/app/src/main/java/com/example/exercisee3movieplayerapp/MainActivity.java
+++ In-class Exercises/Exercise3/app/src/main/java/com/example/exercisee3movieplayerapp/MainActivity.java
@@ -52,4 +52,19 @@ public class MainActivity extends AppCompatActivity {
         // start the video
         video.start();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("lastPositionInVideo", myCurrentPositionInVideo);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // retrieve the position
+        myCurrentPositionInVideo = savedInstanceState.getInt("lastPositionInVideo");
+    }
 }
