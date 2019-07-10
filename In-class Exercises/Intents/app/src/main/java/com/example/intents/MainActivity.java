d451936fc11a49b6050a5eac3f972edd4724160c
Joseph Jeno <joseph.jeno@gmail.com>
2019-07-10 11:54:32 -0400

    implicit intent (opening phone dialer)

---
+++ In-class Exercises/Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    TextView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        welcomeText = findViewById(R.id.myTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
+    }
+}
