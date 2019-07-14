445202f515f3d2336cb1d229878dd41cbc3c39cc
josephjeno <joseph.jeno@gmail.com>
2019-07-14 01:12:18 -0400

    project details followed
    possibly done after review

--- Assignment2/app/src/main/java/com/example/assignment2/DetailsActivity.java
+++ Assignment2/app/src/main/java/com/example/assignment2/DetailsActivity.java
@@ -2,13 +2,56 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    String carWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        ImageView carImage = findViewById(R.id.carImageView);
+        TextView carDetails = findViewById(R.id.carDetailsTextView);
+
+        // Retrieve car from extras
+        Bundle extras = getIntent().getExtras();
+        String car = extras.getString("car");
+
+        // Set specific car details
+        switch(car) {
+            case ("Hatchback"):
+                carImage.setImageResource(R.drawable.iconfinder_hatchback);
+                carDetails.setText(R.string.hatchback_details);
+                carWebsite = getResources().getString(R.string.hatchback_website);
+                break;
+            case("SUV"):
+                carImage.setImageResource(R.drawable.iconfinder_suv);
+                carDetails.setText(R.string.suv_details);
+                carWebsite = getResources().getString(R.string.suv_website);
+                break;
+            default:
+                carImage.setImageResource(R.drawable.iconfinder_sedan);
+                carDetails.setText(R.string.sedan_details);
+                carWebsite = getResources().getString(R.string.sedan_website);
+                break;
+        }
+    }
+
+    public void visitWebsite(View view) {
+        Uri webpage = Uri.parse(carWebsite);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
--- Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
+++ Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton selectedCarRadioButton;
+    CharSequence selectedCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set default selectedCar to Sedan
+        selectedCarRadioButton = findViewById(R.id.sedanRadioButton);
+        selectedCar = selectedCarRadioButton.getText();
+    }
+
+    public void viewCar(View view) {
+        Intent explicitIntent = new Intent(getApplicationContext(), DetailsActivity.class);
+        explicitIntent.putExtra("car", String.valueOf(selectedCar));
+        startActivity(explicitIntent);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        selectedCarRadioButton = (RadioButton) view;
+        selectedCar = selectedCarRadioButton.getText();
+        Log.d("radio_button_clicked", "buttonID is " + selectedCar);
     }
 }
